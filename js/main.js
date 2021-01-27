99c7ec403f2613408a10c7e209ba95ef72f8cab3
Nadia <nadiyaklokova@gmail.com>
2021-01-27 16:26:07 +0100

    escribir la lista de ingredientes

--- js/main.js
+++ js/main.js
@@ -78,13 +78,35 @@ function empezar(e ){
 function soltar (e ) {
     //Indicamos el producto que ha comprado
     //creamos una imagen
-    console.log("jshkjhj");
+    //console.log(e.dataTransfer.getData('Text'));
     imagen = new Image();
     //La imagen será igual a la que arrastramos
     imagen.src = "./img/" + e.dataTransfer.getData('Text') + '.png';
     //añadimos la imagen a la lista
+
+   
+
+
     document.getElementById('lista').appendChild(imagen);
+    var ingrediente = e.dataTransfer.getData('Text');
+    var listaActual = document.getElementById('listaDeIngredientes').innerHTML;
+    console.log(listaActual);
+    console.log(ingrediente);
+    if(listaActual=="") {
+        document.getElementById('listaDeIngredientes').innerHTML =  " " + listaActual.concat(ingrediente) ;
+    }
+     else {
+        document.getElementById('listaDeIngredientes').innerHTML =  " " + listaActual.concat(",", ingrediente) ;
+     }   
+
+    
+
+
+    console.log(listaActual.concat(",", ingrediente));
     } 
+
+
+
     
     //Ya solo queda conectar los Div de lista y comida a las funciones que hemos creado anteriormente:
     
@@ -93,6 +115,13 @@ function soltar (e ) {
     document.getElementById('lista').ondragover = pizza;
     //ondragstart - Al empezar a arrastrar
     document.getElementById('tomate').ondragstart = empezar;
+    document.getElementById('mozarella').ondragstart = empezar;
+    document.getElementById('queso').ondragstart = empezar;
+    document.getElementById('cebolla').ondragstart = empezar;
+    document.getElementById('aceitunas').ondragstart = empezar;
+    document.getElementById('baicon').ondragstart = empezar;
+    document.getElementById('champiñones').ondragstart = empezar;
+    document.getElementById('gorgonzola').ondragstart = empezar;
     //ondragstart - Al empezar a arrastrar
     document.getElementById('albahaca').ondragstart = empezar;
     //ondrop - Al soltar
