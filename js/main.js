c6d101460f76205dfbe2456d65bcf5ece9f7e14a
Nadia <nadiyaklokova@gmail.com>
2021-05-10 14:14:58 +0200

    TouchEvent añadido

--- js/main.js
+++ js/main.js
@@ -48,6 +48,81 @@ function soltar (e ) {
     function reset(){
         location.reload();
       }
+
+
+
+      function startup() {
+        var el = document.getElementsByAtribute("draggable")[0];
+        el.addEventListener("touchstart", handleStart, false);
+        el.addEventListener("touchend", handleEnd, false);
+        el.addEventListener("touchcancel", handleCancel, false);
+        el.addEventListener("touchleave", handleLeave, false);
+        el.addEventListener("touchmove", handleMove, false);
+      }
+
+
+      function handleStart(e ){
+        e.dataTransfer.setData('Text', this.id);
+        e.dataTransfer.effectAllowed = 'move';
+        }
+    
+    function pizza(e ) {
+        e.dataTransfer.dropEffect = 'move';
+        return false;
+        }
+
+    function handleCancel(evt) {
+            evt.preventDefault();
+            var touches = evt.changedTouches;
+          
+        for (var i=0; i<touches.length; i++) {
+              ongoingTouches.splice(i, 1);  // remove it; we're done
+            }
+          }
+    function colorForTouch(touch) {
+        var id = touch.identifier;
+        id = id.toString(16); // make it a hex digit
+        return "#" + id + id + id;
+    }
+    
+    
+    
+    function handleEnd (e ) {
+        e.preventDefault();
+        imagen = new Image();
+        imagen.setAttribute('draggable', false);
+        imagen.src = "./img/" + e.dataTransfer.getData('Text') + '.png';
+        document.getElementById('lista').appendChild(imagen);
+        var ingrediente = e.dataTransfer.getData('Text');
+        var listaActual = document.getElementById('listaDeIngredientes').innerHTML;
+        console.log(listaActual);
+        console.log(ingrediente);
+        if(listaActual=="") {
+            document.getElementById('listaDeIngredientes').innerHTML =  " " + listaActual.concat(ingrediente) ;
+        }
+         else {
+            document.getElementById('listaDeIngredientes').innerHTML =  " " + listaActual.concat("," + "\n" , ingrediente) ;
+         }   
+    
+        console.log(listaActual.concat(",", ingrediente));
+        } 
+    
+    
+        document.getElementById('lista').ondragover = pizza;
+    
+        document.getElementById('tomate').ondragstart = handleStart;
+        document.getElementById('mozarella').ondragstart = handleStart;
+        document.getElementById('queso').ondragstart = handleStart;
+        document.getElementById('cebolla').ondragstart = handleStart;
+        document.getElementById('aceitunas').ondragstart = handleStart;
+        document.getElementById('bacon').ondragstart = handleStart;
+        document.getElementById('champiniones').ondragstart = handleStart;
+        document.getElementById('gorgonzola').ondragstart = handleStart;
+        document.getElementById('albahaca').ondragstart = handleStart;
+    
+        document.getElementById('lista').ondrop = handleEnd;
+    
+
       
 
 
